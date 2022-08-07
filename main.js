62b688856f5389cbfa02310662cb35ad3a381a9a
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-07 11:00:59 +0200

    functions to click and drag to draw and a function to reset colors

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
 let grid = document.querySelector('.grid-container');
-let gridSize = parseInt(prompt("Which size do you want"))
-let block = document.querySelectorAll('.block');
+let root = document.querySelector(':root');
+let resetButton = document.querySelector('.reset-button');
+let rangeInput = document.querySelector('input[type=range]')
 
-for(let i = 1; i <= gridSize * gridSize; i++) {
-    let div = document.createElement('div');
-    // block.style.flexBasis = `calc(100% / ${gridSize})`;
-    div.classList.add('block');
-    grid.append(div);
+
+function initializeGrid() {
+    console.log(rangeInput.value)
+    grid.innerHTML = ``;
+    for(let i = 1; i <= rangeInput.value * rangeInput.value; i++) {
+        let div = document.createElement('div');
+        div.classList.add('block');
+        div.addEventListener('mouseover', changeColor)
+        div.addEventListener('mousedown', changeColorOnClick)
+        div.addEventListener('mouseup', changeMouseValue)
+        grid.appendChild(div);
+    }
+    let widthBlock = 100 / rangeInput.value;
+    root.style.setProperty('--number-of-block', `${widthBlock}%`)
 }
 
+let mouseDown = false;
+function changeColorOnClick(e) {
+    let event = e.currentTarget;
+    event.classList.add('black');
+    mouseDown = true;
+}
 
-
+function changeMouseValue() {
+    mouseDown = false
+}
 
 function changeColor(e) {
     let event = e.currentTarget;
-    console.log(e, e.currenttarget)
-    event.classList.add('black');
+    if(mouseDown === true) event.classList.add('black')
 }
 
 
-block.forEach(element => element.addEventListener('mouseover', function(e) {changeColor(e)}));
+
+function resetColor() {
+    let block = document.querySelectorAll('.block');
+    block.forEach(el => el.classList.remove('black'))
+}
+
+rangeInput.addEventListener('DOMContentLoaded', initializeGrid)
+rangeInput.addEventListener('change', initializeGrid)
+resetButton.addEventListener('click', resetColor);
