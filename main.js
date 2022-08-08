4233665dcd81436daa36daaec3b462f8ba419b60
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-08 15:59:59 +0200

    calculate a random number between 0 and 255 to reduce redundancy

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ let rangeInput = document.querySelector('input[type=range]');
 let colorInput = document.querySelector('input[type=color');
 let labelSize = document.querySelector('#label-size');
 let randomButton = document.querySelector('.button-random-color');
+let darkerButton = document.querySelector('.darker-button');
+let lighterButton = document.querySelector('.lighter-button');
 
+// let darkerButtonState = false;
+// let lighterButtonState = false;
 let mouseDown = false;
 let randomButtonStatus = false;
 let color = colorInput.value;
@@ -31,9 +35,9 @@ function initializeGrid() {
 function changeColorOnClick(e) {
     let event = e.currentTarget;
     if(randomButtonStatus) {
-        let randomRed = Math.floor(Math.random()*255);
-        let randomGreen = Math.floor(Math.random()*255);
-        let randomBlue = Math.floor(Math.random()*255);
+        let randomRed = randomTo255();
+        let randomGreen = randomTo255();
+        let randomBlue = randomTo255();
         event.style.backgroundColor = `rgba(${randomRed}, ${randomGreen}, ${randomBlue}, 1)`;
     } else event.style.backgroundColor = color;
 
@@ -41,34 +45,66 @@ function changeColorOnClick(e) {
 }
 
 function changeMouseValue() {
-    mouseDown = false
+    mouseDown = false;
 }
 
 function changeColor(e) {
     let event = e.currentTarget;
     if(mouseDown){
         if(randomButtonStatus){
-            let randomRed = Math.floor(Math.random()*255);
-            let randomGreen = Math.floor(Math.random()*255);
-            let randomBlue = Math.floor(Math.random()*255);
+            let randomRed = randomTo255();
+            let randomGreen = randomTo255();
+            let randomBlue = randomTo255();
             event.style.backgroundColor = `rgba(${randomRed}, ${randomGreen}, ${randomBlue}, 1)`;
         } else if (mouseDown === true) event.style.backgroundColor = color;
     }
 }
 
+
+
+function randomTo255() {
+    return Math.floor(Math.random()*255);
+}
+
+
+
 function randomButtonStatusChange() {
     if(randomButtonStatus) {
         randomButtonStatus = false;
         randomButton.textContent = "Active Random Color";
-    }else {
+    } else {
         randomButtonStatus = true
         randomButton.textContent = "Deactive Random Color";
     }
 }
 
+// function darkerButtonStateChange() {
+//     if(darkerButtonState) {
+//         darkerButtonState = false;
+//         darkerButton.textContent = "Active";
+//     } else {
+//         darkerButtonState = true
+//         lighterButtonState = false;
+//         randomButtonStatus = false
+//         darkerButton.textContent = "Deactive";
+//     }
+// }
+
+// function lightherButtonStateChange() {
+//     if(lighterButtonState) {
+//         lighterButtonState = false;
+//         lighterButton.textContent = "Active";
+//     } else {
+//         lighterButtonState = true
+//         darkerButtonState = false;
+//         randomButtonStatus = false;
+//         lighterButton.textContent = "Deactive";
+//     }
+// }
+
 function resetColor() {
     let block = document.querySelectorAll('.block');
-    block.forEach(el => el.style.backgroundColor = "white")
+    block.forEach(el => el.style.backgroundColor = "rgba(255, 255, 255)")
 }
 
 
@@ -80,3 +116,5 @@ colorInput.addEventListener('input', function() {
     color = colorInput.value;
     rangeInput.style.accentColor = color;
 })
+// darkerButton.addEventListener('click', darkerButtonStateChange);
+// lighterButton.addEventListener('click', lightherButtonStateChange);
